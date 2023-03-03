21f4b37279e3977ad3f56924da8dec9ea5a4b4f2
MarcosVxny <Oliveira.marcos1280@gmail.com>
2023-03-02 21:27:21 -0300

    Interface

--- src/contarepository/ContaRepository.java
+++ src/contarepository/ContaRepository.java
@@ -1,10 +1,5 @@
 package contarepository;
 
-public class ContaRepository {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public interface ContaRepository {
 
 }
